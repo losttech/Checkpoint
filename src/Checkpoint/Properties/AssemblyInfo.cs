dc6619e32398c843a8f18e6a32e4dd7acab7d80f
Victor Milovanov <lost@losttech.software>
2016-10-01 14:40:11 -0700

    empty project

---
+++ src/Checkpoint/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Checkpoint")]
+[assembly: AssemblyDescription("Provides asynchronous checkpoint functionality, such as asynchronous autosave for doc")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lost Tech")]
+[assembly: AssemblyProduct("Checkpoint")]
+[assembly: AssemblyCopyright("Copyright © Lost Tech 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0")]
