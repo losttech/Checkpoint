21c8ab10ce181a7ce929d3bbf135a6a0a5f0091d
Victor Nova <lost@losttech.software>
2022-06-01 10:52:33 -0700

    added tests for TaskException and FatalError

---
+++ test/ErrorHandling.cs
@@ -0,0 +1,41 @@
+﻿namespace LostTech.Checkpoint;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Xunit;
+public class ErrorHandling {
+    [Fact]
+    public async Task AggregatesErrors() {
+        var chain = new AsyncChainService();
+        for (int i = 0; i < 10; i++)
+            chain.Chain(async () => {
+                await Task.Delay(100);
+                throw new ArgumentOutOfRangeException(nameof(i), i, "test error");
+            });
+        await chain.DisposeAsync();
+        Assert.NotNull(chain.FatalErrors);
+        Assert.Equal(10, chain.FatalErrors.InnerExceptions.Count);
+    }
+
+    [Fact]
+    public async Task Observing() {
+        var chain = new AsyncChainService();
+        int observed = 0;
+        chain.TaskException += (_, e) => {
+            if (e.Exception.InnerException is ArgumentOutOfRangeException) {
+                e.SetObserved();
+                observed++;
+            }
+        };
+        for (int i = 0; i < 10; i++)
+            chain.Chain(async () => {
+                await Task.Delay(100);
+                throw new ArgumentOutOfRangeException(nameof(i), i, "test error");
+            });
+        var waitStopwatch = Stopwatch.StartNew();
+        await chain.DisposeAsync();
+        Assert.Null(chain.FatalErrors);
+        Assert.Equal(10, observed);
+    }
+}
